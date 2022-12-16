f427a7e230a3cad2f39ae2e6604aaf3e00bb6abc
Andreea Mirea <alecsia0811@gmail.com>
2022-12-16 18:22:49 +0200

    aa

---
+++ AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Mirea_Andreea_Lab7;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -20,5 +20,15 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+    private void CounterBtn_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void CounterBtn_Clicked_1(object sender, EventArgs e)
+    {
+
+    }
 }
 
