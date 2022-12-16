3d6b1ffd9a534171ae535af0a7f62179f2f2f9d3
Andreea Mirea <alecsia0811@gmail.com>
2022-12-16 17:07:18 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Mirea_Andreea_Lab7;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Mirea_Andreea_Lab7;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
